c3e1b87b396a7209d09ab2f770a723c574eed092
ee268 <sh33dhl@qq.com>
2026-07-16 00:24:07 +0800

    style: 优化about的部分布局为卡片式

--- components/views/aboutpage.cpp
+++ components/views/aboutpage.cpp
@@ -10,6 +10,7 @@
 #include <QDebug>
 
 #include "ElaImageCard.h"
+#include "ElaTheme.h"
 
 AboutPage::AboutPage(QWidget *parent)
     : ElaDialog(parent)
@@ -29,45 +30,59 @@ void AboutPage::initDialog()
     setIsFixedSize(true);
 
     QImage logoImg = QImage(":/resource/image/logo/OceanLink.png");
-    float ratio = 0.45;
+    float ratio = 0.40;
     int w = logoImg.width() * ratio;
     int h = logoImg.height() * ratio;
     ElaImageCard* logoCard = new ElaImageCard(this);
     logoCard->setCardImage(logoImg);
     logoCard->setFixedSize(QSize(w, h));
 
-    IconText* versionInfo = new IconText(this);
-    versionInfo->setText(QString("版本: %1 (64位)").arg(PROJECT_VERSION));
-    versionInfo->setPixelSize(16);
-    versionInfo->setTextColor(QColor(17, 141, 240));
+    QString versionText = QString("版本: %1 (64位)").arg(PROJECT_VERSION);
+    AboutInfoWidget* versionInfo = new AboutInfoWidget(
+        QIcon(":/resource/image/about/code-slash-outline.png"), versionText, this);
+    versionInfo->setFixedSize(this->width() - 40, versionInfo->getImplicitIconSize() + 20);
     versionInfo->setTextBold(true);
+    versionInfo->setTextColor(QColor("#1e87ef"));
 
-    IconText* copyrightInfo = new IconText(this);
-
+    QString copyrightText;
     QString beginYear = "2026";
     QString curYear = QString::number(QDateTime::currentDateTime().date().year());
     if (curYear == beginYear) {
         curYear = "now";
     }
-    copyrightInfo->setText(QString("Copyright © %1-%2 ee268.")
+    copyrightText = QString("Copyright © %1-%2 ee268.")
                                .arg(beginYear)
-                               .arg(curYear));
+                               .arg(curYear);
+    AboutInfoWidget* copyrightInfo = new AboutInfoWidget(
+        QIcon(":/resource/image/about/easel-outline.png"), copyrightText, this);
+    copyrightInfo->setFixedSize(versionInfo->size());
 
-    IconText* licenseInfo = new IconText("MIT LICENSE", this);
-    licenseInfo->setIcon(QIcon(":/resource/image/about/document-text-outline.svg"));
+    AboutInfoWidget* licenseInfo = new AboutInfoWidget(
+        QIcon(":/resource/image/about/shield-checkmark-outline.png"), "MIT LICENSE", this);
+    licenseInfo->setFixedSize(versionInfo->size());
+    licenseInfo->setTextColor(QColor("#1e87ef"));
+
+    QWidget* infoWid = new QWidget(this);
+    QVBoxLayout* infoWidLayout = new QVBoxLayout(infoWid);
+
+    infoWidLayout->setContentsMargins(0, 0, 0, 0);
+    infoWidLayout->setSpacing(10);
+    infoWidLayout->addWidget(versionInfo, 0, Qt::AlignHCenter);
+    infoWidLayout->addWidget(copyrightInfo, 1, Qt::AlignHCenter);
+    infoWidLayout->addWidget(licenseInfo, 2, Qt::AlignHCenter);
+    infoWid->setLayout(infoWidLayout);
 
     IconText* techInfo = new IconText("基于Qt 5.15 Widgets + ElaWidgetTools搭建", this);
-    IconText* githubInfo = new IconText("https://github.com/ee268/XunLink", this);
+    IconText* githubInfo = new IconText(QString("https://github.com/ee268/%1").arg(PROjECT_NAME), this);
     githubInfo->setIcon(QIcon(":/resource/image/about/logo-github.svg"));
+    githubInfo->setTextColor(QColor("#2789f3"));
 
     QVBoxLayout* mainLayout = new QVBoxLayout(this);
-    mainLayout->setContentsMargins(0, 0, 0, 15);
+    mainLayout->setContentsMargins(0, 15, 0, 15);
     mainLayout->addWidget(logoCard, 0, Qt::AlignCenter);
-    mainLayout->addSpacerItem(new QSpacerItem(0, 10, QSizePolicy::Expanding, QSizePolicy::Minimum));
-    mainLayout->addWidget(versionInfo, 1, Qt::AlignHCenter);
-    mainLayout->addWidget(copyrightInfo, 2, Qt::AlignHCenter);
-    mainLayout->addWidget(licenseInfo, 3, Qt::AlignHCenter);
-    mainLayout->addSpacerItem(new QSpacerItem(0, 150, QSizePolicy::Expanding, QSizePolicy::Minimum));
+    mainLayout->addSpacerItem(new QSpacerItem(0, 10, QSizePolicy::Minimum, QSizePolicy::Expanding));
+    mainLayout->addWidget(infoWid);
+    mainLayout->addSpacerItem(new QSpacerItem(0, 80, QSizePolicy::Minimum, QSizePolicy::Expanding));
     mainLayout->addWidget(techInfo, 4, Qt::AlignHCenter);
     mainLayout->addWidget(githubInfo, 5, Qt::AlignHCenter);
     mainLayout->setSpacing(5);
@@ -75,4 +90,91 @@ void AboutPage::initDialog()
     this->setLayout(mainLayout);
 }
 
+AboutInfoWidget::AboutInfoWidget(const QIcon &icon, const QString &text, QWidget *parent)
+    : QWidget(parent)
+    , _icon(icon)
+    , _text(text)
+    , _iconBgColor(QColor(227, 239, 254))
+    , _iconColor(QColor("#2896f9"))
+    , _iconSize(25)
+    , _leftMargin(15)
+    , _spacing(15)
+    , _borderRadius(10)
+{
+    _textFont.setPixelSize(13);
+}
 
+int AboutInfoWidget::getImplicitIconSize() const
+{
+    return _iconSize + 5;
+}
+
+void AboutInfoWidget::setTextColor(const QColor& color)
+{
+    _textColor = color;
+    update();
+}
+
+void AboutInfoWidget::setTextBold(bool isBold)
+{
+    _textFont.setBold(isBold);
+    update();
+}
+
+void AboutInfoWidget::paintEvent(QPaintEvent *event)
+{
+    ElaThemeType::ThemeMode themeMode = eTheme->getThemeMode();
+    QPainter painter(this);
+    QRect thisRect = this->rect();
+    painter.setRenderHints(QPainter::SmoothPixmapTransform | QPainter::Antialiasing);
+
+    painter.setPen(ElaThemeColor(themeMode, BasicBorder));
+    painter.setBrush(ElaThemeColor(themeMode, BasicBase));
+    painter.drawRoundedRect(thisRect.adjusted(1, 1, -1, -1), _borderRadius, _borderRadius);
+
+    QRect iconRect(thisRect.left() + _leftMargin,
+                   thisRect.top() + (thisRect.height() - (_iconSize + 5)) / 2,
+                   _iconSize + 5, _iconSize + 5);
+    QPixmap iconPixmap = _icon.pixmap(_iconSize, _iconSize);
+    QPainter pmPainter(&iconPixmap);
+    pmPainter.setCompositionMode(QPainter::CompositionMode_SourceAtop);
+    pmPainter.fillRect(iconPixmap.rect(), _iconColor);
+    pmPainter.end();
+
+    painter.setPen(Qt::NoPen);
+    if (themeMode == ElaThemeType::Light) {
+        _iconBgColor = QColor(227, 239, 254);
+    }
+    else {
+        _iconBgColor = QColor(30, 55, 90);
+    }
+    painter.setBrush(_iconBgColor);
+    painter.drawRoundedRect(iconRect, _borderRadius - 3, _borderRadius - 3);
+    painter.drawPixmap(iconRect.adjusted(5, 5, -5, -5), iconPixmap);
+
+    if (_textColor.isValid()) {
+        painter.setPen(_textColor);
+    }
+    else {
+        painter.setPen(ElaThemeColor(themeMode, BasicText));
+    }
+
+    QFontMetrics fm(_textFont);
+    QRect textRect(iconRect.right() + _spacing,
+                   iconRect.top() + (iconRect.height() - fm.height()) / 2,
+                   thisRect.right() - iconRect.right() - _leftMargin - _spacing,
+                   fm.height());
+
+    painter.setFont(_textFont);
+    painter.drawText(textRect, _text);
+}
+
+QSize AboutInfoWidget::sizeHint() const
+{
+    QFontMetrics fm(_textFont);
+
+    int w = _leftMargin + _iconSize + 5 + _spacing + fm.horizontalAdvance(_text);
+    int h = _iconSize + 5 + 20;
+
+    return QSize(w, h);
+}
--- components/views/aboutpage.h
+++ components/views/aboutpage.h
@@ -3,6 +3,37 @@
 
 #include "ElaDialog.h"
 
+#include <QIcon>
+
+class AboutInfoWidget : public QWidget
+{
+public:
+    explicit AboutInfoWidget(const QIcon& icon, const QString& text, QWidget* parent = nullptr);
+
+    int getImplicitIconSize() const;
+
+    void setTextColor(const QColor& color);
+
+    void setTextBold(bool isBold);
+
+protected:
+    void paintEvent(QPaintEvent * event) override;
+    QSize sizeHint() const override;
+
+private:
+    QIcon _icon;
+    QString _text;
+    QFont _textFont;
+    QColor _iconBgColor;
+    QColor _iconColor;
+    int _iconSize;
+    QColor _textColor;
+
+    int _leftMargin;
+    int _spacing;
+    int _borderRadius;
+};
+
 class AboutPage : public ElaDialog
 {
     Q_OBJECT
